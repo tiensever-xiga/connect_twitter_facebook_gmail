a4f0c36d977f6ab421d49a8a8e9fc93ea70e8762
tiensever <tiennh@xiga.info>
2011-08-20 14:43:28 +0700

    error connect facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :twitter, 'KdvK59ChQiKcnunWd5Q3jA', 'NH8fCud00ljotRxu6UlwOVJ4F9EYAmqeFp1ecQeioU'
+  provider :facebook, '248251888542589', '3f34e4d110620f60ad3765fba752f1bc'
 end
